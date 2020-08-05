195bac017af4cb92b09cf5f0f24b57c32ded5a4a
simran rani <simran.rani@cognizant.com>
2020-08-05 12:40:31 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super();
+    this.state = {
+      employees: [],
+    };
+  }
+
+  componentDidMount() {
+    this.getEmployeeDetails();
+  }
+  getEmployeeDetails() {
+    Axios.get("http://dummy.restapiexample.com/api/v1/employees")
+      .then((response) => {
+        this.setState({ employees: response.data.data });
+        console.log(response.data.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Employee Details</h1>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Id</th>
+              <th>Name</th>
+              <th>Salary</th>
+              <th>Age</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.employees.map(function (item, key) {
+              return (
+                <tr key={key}>
+                  <td>{item.id}</td>
+                  <td>{item.employee_name}</td>
+                  <td>{item.employee_salary}</td>
+                  <td>{item.employee_age}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
 }
 
 export default App;
